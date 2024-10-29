652915aae6be87736bf72682904ea7234776f0c7
Felipe Adelungue Gasparotto <felipe.gasparotto@sptech.school>
2024-10-29 14:58:45 -0300

    Feature: V2 script build

--- scriptBuild.sh
+++ scriptBuild.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+#instalando python
+echo "Bem vindo a TagTech, script de instalação do ambiente
+
+ ___  _   __  ___  ___ __  _ _ 
+|_ _|/ \ / _||_ _|| __/ _|| U |
+ | || o ( |_n | | | _( (_ |   |
+ |_||_n_|\__/ |_| |___\__||_n_|
+                               
+
+"
+
+which python3
+if [ $? = /usr/bin/python3 ] 
+  then echo "Python3 instalado"
+  else sudo apt install python3
+fi
+
+sudo apt-install python3-psutil
+sudo apt-install python3-python-dotenv
+sudo apt-install python3-os
+sudo apt-install python3-requests
+sudo apt-install python3-json
+sudo apt-install python3-time
+sudo apt-install python3-platform
+sudo apt-install python3-socket
+sudo apt-install python3-pandas
+sudo apt-install python3-random
+sudo apt-install python3-boto3
+sudo apt-install python3-holidays
+
+
+#configurando bibliotecas python
+
 # configurando chaves GPG docker
 
 sudo apt-get update
@@ -22,3 +55,33 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 #adicionando o usuario no grupo sudo 
 sudo usermod -aG docker $USER  
 newgrp docker
+
+echo "Comandos feitos, fim do processo"
+
+echo "Iniciando DOCKER"
+
+docker -v
+
+docker pull mysql
+
+docker run -d -p 3306:3306 --name db -e "MYSQL_ROOT_PASSWORD=urubu100" mysql
+
+#sudo docker start db
+
+#sudo docker exec -it db bash
+
+which node -v
+if [ $? != "" ]
+  then echo "Node instalado"
+  else curl https://deb.nodesource.com/setup_16.x | sudo -E bash - 
+  sudo apt install -y nodejs
+fi
+
+git clone https://github.com/Tagg-Tech/Development.git && git clone https://github.com/Tagg-Tech/DATA_ANALYTICS.git && git clone https://github.com/Tagg-Tech/Scripts_EC2.git
+
+cd /Development/web-data-viz
+
+npm i && npm start
+
+echo "Aproveite!"
+
