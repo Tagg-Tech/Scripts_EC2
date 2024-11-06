05a20d08db8f8c75dddf69f4f86f2caedaaef641
Ivanrangelpm <ivan.marcolin@sptech.school>
2024-11-05 22:23:46 -0300

    testando py

---
+++ build.sh
@@ -0,0 +1,9 @@
+echo "Bem vindo a TagTech, script de instalação do ambiente
+
+ ___  _   __  ___  ___ __  _ _ 
+|_ _|/ \ / _||_ _|| __/ _|| U |
+ | || o ( |_n | | | _( (_ |   |
+ |_||_n_|\__/ |_| |___\__||_n_|
+                               
+
+"
---
+++ parte.sh
@@ -0,0 +1,21 @@
+echo "Bem vindo a TagTech, script de instalação do ambiente
+
+ ___  _   __  ___  ___ __  _ _ 
+|_ _|/ \ / _||_ _|| __/ _|| U |
+ | || o ( |_n | | | _( (_ |   |
+ |_||_n_|\__/ |_| |___\__||_n_|
+                               
+
+"
+
+which python3
+if [ $? -eq 0 ]; then
+  echo "Python3 instalado"
+else
+  echo "Instalando Python..."
+  sudo apt install python3
+fi
+
+
+
+sudo apt install -y python3-pip python3-psutil python3-dotenv python3-requests python3-pandas python3-boto3
